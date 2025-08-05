39b0a6c9f2b0a8644a2de3dbc8182a868c0c16af
chaejieun <pettojim@naver.com>
2025-08-05 16:36:16 +0900

    chore: apply updated .gitignore rules

---
+++ src/main/java/com/myarchive/backend/MyarchiveApplication.java
@@ -0,0 +1,13 @@
+package com.myarchive.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyarchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyarchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myarchive/backend/MyarchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myarchive.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyarchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
